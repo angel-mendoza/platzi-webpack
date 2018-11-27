7bf943adb4a0fbdaf9bd61091987b5c04a926c93
Angel Mendoza <mendozaangel003@gmail.com>
2018-11-27 11:08:11 -0500

    se empezo a trabajar con estilos

---
+++ css-style-loader/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, './index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: './bundle.js'
+    },
+    module:{
+    	rules: [
+    		{
+    			test: /\.css$/,
+    			use: [
+    				'style-loader', 'css-loader'
+    			]
+    		}
+    	]
+    }
+}
